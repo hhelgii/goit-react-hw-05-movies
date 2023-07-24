5f49d4d89067a05fc6b7cb98e723f70cbd196ff7
hhelgii <opruska2004@gmail.com>
2023-07-25 00:00:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/hhelgii.github.io'>
+  <BrowserRouter basename='/goit-react-hw-05-movies'>
     <App />
   </BrowserRouter>
 );
